0db01dc8144a9c8e9febe60f47387332284e0965
Oleg Kovalov <oleg@hey.com>
2021-10-27 23:18:05 +0200

    Small updates

--- acmd.go
+++ acmd.go
@@ -45,8 +45,8 @@ type Config struct {
 	// Args passed to the executable, if nil os.Args[1:] will be used.
 	Args []string
 
-	// Usage of the application, if nil flag.Usage will be used,
-	Usage func()
+	// Usage of the application, if nil default will be used,
+	Usage func(name string, cmds []Command)
 }
 
 // RunnerOf creates a Runner.
@@ -60,29 +60,31 @@ func RunnerOf(cmds []Command, cfg Config) *Runner {
 }
 
 func (r *Runner) init() error {
-	args := r.cfg.Args
-	if len(args) == 0 {
-		args = os.Args[1:]
+	r.args = r.cfg.Args
+	if len(r.args) == 0 {
+		r.args = os.Args[1:]
 	}
-	if len(r.cfg.Args) == 0 {
-		return errors.New("acmd: no args provided")
+	if len(r.args) == 0 {
+		return errors.New("no args provided")
 	}
 
-	ctx := r.cfg.Context
-	if ctx == nil {
-		var cancel context.CancelFunc
-		r.cfg.Context, cancel = signal.NotifyContext(context.Background(), os.Interrupt)
-		// TODO: fix
-		defer cancel()
+	r.ctx = r.cfg.Context
+	if r.ctx == nil {
+		// ok to ignore cancel func because os.Interrupt is already almost os.Exit
+		r.ctx, _ = signal.NotifyContext(context.Background(), os.Interrupt)
 	}
 
 	names := make(map[string]struct{})
 	for _, cmd := range r.cmds {
-		if cmd.Name == "help" || cmd.Name == "version" {
-			return fmt.Errorf("acmd: command name %q is reserved", cmd.Name)
+		switch {
+		case cmd.Do == nil:
+			return fmt.Errorf("command %q function cannot be nil", cmd.Name)
+		case cmd.Name == "help" || cmd.Name == "version":
+			return fmt.Errorf("command %q is reserved", cmd.Name)
 		}
+
 		if _, ok := names[cmd.Name]; ok {
-			return fmt.Errorf("acmd: duplicate command %q", cmd.Name)
+			return fmt.Errorf("duplicate command %q", cmd.Name)
 		}
 		names[cmd.Name] = struct{}{}
 	}
@@ -108,7 +110,7 @@ func (r *Runner) run() error {
 	cmd, params := r.args[0], r.args[1:]
 	switch {
 	case cmd == "help":
-		r.cfg.Usage()
+		r.cfg.Usage(r.cfg.AppName, r.cmds)
 		return nil
 	case cmd == "version":
 		fmt.Printf("%s version: %s\n", r.cfg.AppName, r.cfg.Version)
@@ -120,5 +122,5 @@ func (r *Runner) run() error {
 			return c.Do(r.ctx, params)
 		}
 	}
-	return fmt.Errorf("acmd: no such command %q", cmd)
+	return fmt.Errorf("no such command %q", cmd)
 }
